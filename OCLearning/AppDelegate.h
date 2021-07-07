d1a5336ab8c1ca37a255ea6b1bbfbbc7bb0120c7
quanrenyi <quanrenyi@yy.com>
2021-07-07 13:56:56 +0800

    Initial Commit

---
+++ OCLearning/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OCLearning
+//
+//  Created by yamyee on 2021/7/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
